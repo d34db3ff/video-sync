53f62e62e46f540cd94aa5a1b9217f67feac2eb0
Minty <the@stella.observer>
2024-03-29 23:02:51 +0200

    init release

--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,18 @@
-/**
- * Welcome to Cloudflare Workers! This is your first Durable Objects application.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your Durable Object in action
- * - Run `npm run deploy` to publish your application
- *
- * Learn more at https://developers.cloudflare.com/durable-objects
- */
-
-/**
- * Associate bindings declared in wrangler.toml with the TypeScript type system
- */
 export interface Env {
-	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: KVNamespace;
-	//
-	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-	MY_DURABLE_OBJECT: DurableObjectNamespace;
-	//
-	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-	// MY_BUCKET: R2Bucket;
-	//
-	// Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
-	// MY_SERVICE: Fetcher;
-	//
-	// Example binding to a Queue. Learn more at https://developers.cloudflare.com/queues/javascript-apis/
-	// MY_QUEUE: Queue;
+	WEBSOCKET_SERVERS: DurableObjectNamespace;
 }
 
-/** A Durable Object's behavior is defined in an exported Javascript class */
-export class MyDurableObject {
+export class WebSocketServer {
+	state: DurableObjectState;
 	/**
-	 * The constructor is invoked once upon creation of the Durable Object, i.e. the first call to
-	 * 	`DurableObjectStub::get` for a given identifier
+	 * The constructor is invoked every time the Durable Object is recovered from hibernation
 	 *
 	 * @param state - The interface for interacting with Durable Object state
 	 * @param env - The interface to reference bindings declared in wrangler.toml
 	 */
-	constructor(state: DurableObjectState, env: Env) {}
+	constructor(state: DurableObjectState, env: Env) {
+		this.state = state;
+	}
 
 	/**
 	 * The Durable Object fetch handler will be invoked when a Durable Object instance receives a
@@ -47,32 +22,42 @@ export class MyDurableObject {
 	 * @returns The response to be sent back to the Worker
 	 */
 	async fetch(request: Request): Promise<Response> {
-		return new Response('Hello World');
+		// Check if the request is a WebSocket upgrade request
+		const upgradeHeader = request.headers.get('Upgrade');
+		if (!upgradeHeader || upgradeHeader !== 'websocket') {
+			return new Response('Durable Object expected Upgrade: websocket', { status: 426 });
+		}
+
+		// Creates two ends of a WebSocket connection.
+		const webSocketPair = new WebSocketPair();
+		const [client, server] = Object.values(webSocketPair);
+		this.state.acceptWebSocket(server);
+
+		return new Response(null, {
+			status: 101,
+			webSocket: client,
+		});
 	}
+
+	async webSocketMessage(ws: WebSocket, message: ArrayBuffer | string) {
+		// Simply broadcast the message to all connected clients in the room
+		this.state.getWebSockets().forEach((client) => {
+		  if (client !== ws)
+			client.send(`${message}`);
+		});
+	  }
+	
+	  async webSocketClose(ws: WebSocket, code: number, reason: string, wasClean: boolean) {
+		// If the client closes the connection, the runtime will invoke the webSocketClose() handler.
+		ws.close(code, "Durable Object is closing WebSocket");
+	  }
 }
 
 export default {
-	/**
-	 * This is the standard fetch handler for a Cloudflare Worker
-	 *
-	 * @param request - The request submitted to the Worker from the client
-	 * @param env - The interface to reference bindings declared in wrangler.toml
-	 * @param ctx - The execution context of the Worker
-	 * @returns The response to be sent back to the client
-	 */
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		// We will create a `DurableObjectId` using the pathname from the Worker request
-		// This id refers to a unique instance of our 'MyDurableObject' class above
-		let id: DurableObjectId = env.MY_DURABLE_OBJECT.idFromName(new URL(request.url).pathname);
-
-		// This stub creates a communication channel with the Durable Object instance
-		// The Durable Object constructor will be invoked upon the first call for a given id
-		let stub: DurableObjectStub = env.MY_DURABLE_OBJECT.get(id);
-
-		// We call `fetch()` on the stub to send a request to the Durable Object instance
-		// The Durable Object instance will invoke its fetch handler to handle the request
-		let response = await stub.fetch(request);
-
-		return response;
+		// Each Durable Object is identified by a unique ROOM_ID in the URL path
+		let id: DurableObjectId = env.WEBSOCKET_SERVERS.idFromName(new URL(request.url).pathname);
+		let stub: DurableObjectStub = env.WEBSOCKET_SERVERS.get(id);
+		return stub.fetch(request);
 	},
 };
